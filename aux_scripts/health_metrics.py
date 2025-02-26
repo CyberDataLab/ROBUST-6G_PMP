ad74051188661e2e88c8af0a8f1f0b8ec199f409
LongJeffreySilver <87135689+LongJeffreySilver@users.noreply.github.com>
2025-02-26 09:59:14 +0100

    Machine_ID and Telegraf update
    The number of comments has also been reduced

--- aux_scripts/health_metrics.py
+++ aux_scripts/health_metrics.py
@@ -2,10 +2,10 @@
 import psutil
 
 def main():
-    # "Calentar" la medición de CPU
-    psutil.cpu_percent(interval=None)  # sin usar el valor
-    cpu_usage = psutil.cpu_percent(interval=1)  # segunda llamada, valor real
-    mem_info = psutil.virtual_memory().available / (1024*1024)  # en MB
+    # “Warm up” the CPU measurement using the value of the second call because it may give errors in the first call.
+    psutil.cpu_percent(interval=None)  
+    cpu_usage = psutil.cpu_percent(interval=1)  
+    mem_info = psutil.virtual_memory().available / (1024*1024)  # in MB
     print(f'{{"cpu_usage":{cpu_usage},"memory_mb":{mem_info}}}')
 
 if __name__ == "__main__":
