66a81b0d0768f8909bce7623d2c25e54c91bdf2f
LongJeffreySilver <87135689+LongJeffreySilver@users.noreply.github.com>
2025-02-26 10:28:11 +0100

    Machine_ID

---
+++ machine_id.py
@@ -0,0 +1,26 @@
+import platform
+import subprocess
+import sys
+import os
+
+def get_device_id():
+    system_name = platform.system().lower()
+    if system_name == "windows":
+        mid_binary = "./machine_id/mid_windows/mid.exe"
+    else:
+        mid_binary = "./machine_id/mid_linux_macos/mid"
+
+    try:
+        output = subprocess.check_output([mid_binary], stderr=subprocess.STDOUT)
+        device_id = output.decode("utf-8").strip()
+        return device_id
+    except FileNotFoundError:
+        print(f"Binary not found '{mid_binary}'.")
+        sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Error while executing '{mid_binary}': {e.output.decode('utf-8')}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    device_id = get_device_id()
+    print(f"{device_id}")
